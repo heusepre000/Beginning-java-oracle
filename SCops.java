429111387789b04456d828576153fd439973a1fd
Preston  Heuser <37519044+heusepre000@users.noreply.github.com>
2019-03-02 11:25:23 -0800

    Add files via upload

---
+++ SCops.java
@@ -0,0 +1,23 @@
+// Demonstrate the short circuit operators.
+public class SCops {
+    public static void main(String args[]) {
+        int n, d, q;
+
+        n = 10;
+        d = 2;
+        if (d != 0 && (n % d) == 0)
+            System.out.println(d + " is a factor of " + n);
+
+        d = 0; // now, set d to zero
+
+        // Since d is zero, the second operand is not evaluated.
+        if (d != 0 && (n % d) == 0)
+            System.out.println(d + " is a factor of " + n);
+
+        /* I'll do the same thing without the short circuit operator.
+        This will cause a divide-by-zero error.
+         */
+        if (d != 0 & (n % d) == 0)
+            System.out.println(d + " is a factor of " + n);
+    }
+}
