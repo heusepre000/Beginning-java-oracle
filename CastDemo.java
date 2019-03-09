0d60ec0a2e720a9f7eace6795b13a14f3f4f193e
Preston  Heuser <37519044+heusepre000@users.noreply.github.com>
2019-03-09 10:37:36 -0800

    Add files via upload

---
+++ CastDemo.java
@@ -0,0 +1,23 @@
+//Demonstrate casting.
+public class CastDemo {
+    public static void main(String args []) {
+        double x, y;
+        byte b;
+        int i;
+        char ch;
+
+        x = 10.0;
+        y = 3.0;
+
+        i = (int) (x / y); // cast double int
+        System.out.println("Integer outcome of x / y: " + i);
+
+        i = 100;
+        b = (byte) i;
+        System.out.println("Value of b: " + b);
+
+        b = 88; // ASCII code for x
+        ch = (char) b;
+        System.out.println("ch: " + ch);
+    }
+}
