c8b854ff42c74ddde87c8433cc83ee86e08bf4e6
Preston  Heuser <37519044+heusepre000@users.noreply.github.com>
2019-03-09 11:41:15 -0800

    Add files via upload

---
+++ LogicalOpTable.java
@@ -0,0 +1,30 @@
+public class LogicalOpTable {
+    public static void main(String args []) {
+
+        boolean p, q;
+
+        System.out.println("p\t\tQ\t\tAND\t\tOR\t\tXOR\t\tNOT");
+
+        p = true; q = true;
+        System.out.print(p + "\t" + q +"\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (!p));
+
+        p = true; q = false;
+        System.out.print(p + "\t" + q +"\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (!p));
+
+        p = false; q = true;
+        System.out.print(p + "\t" + q +"\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (!p));
+
+        p = false; q = false;
+        System.out.print(p + "\t" + q +"\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (!p));
+
+    }
+
+}
