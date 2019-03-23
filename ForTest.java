9ee67bfe4609ddc8b690af733ba059e8a32a2a86
Preston  Heuser <37519044+heusepre000@users.noreply.github.com>
2019-03-23 11:29:34 -0700

    Add files via upload

---
+++ ForTest.java
@@ -0,0 +1,12 @@
+public class ForTest {
+    public static void main(String args[])
+    throws java.io.IOException {
+
+        int i;
+
+        System.out.println("Press S to stop.");
+
+        for (i = 0; (char) System.in.read() != 'S'; i++)
+            System.out.println("Pass #" + i);
+    }
+}
