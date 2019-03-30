1ecd5cabe400d56f694fa4c663c4f001758faa9a
Preston  Heuser <37519044+heusepre000@users.noreply.github.com>
2019-03-30 10:33:07 -0700

    Add files via upload

---
+++ WhileDemo2.java
@@ -0,0 +1,13 @@
+// Demonstrate the while loop
+public class WhileDemo2 {
+    public static void main(String args[]) {
+        char ch;
+
+        // print the alphabet
+        ch = 'a';
+        while (ch <= 'z') {
+            System.out.println(ch);
+            ch++;
+        }
+    }
+}
