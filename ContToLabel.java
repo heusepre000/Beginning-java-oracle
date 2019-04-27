1945149c3026125cf916838406af6216675c144a
Preston  Heuser <37519044+heusepre000@users.noreply.github.com>
2019-04-27 11:46:51 -0700

    Add files via upload

---
+++ ContToLabel.java
@@ -0,0 +1,16 @@
+// use continue with a label
+public class ContToLabel {
+    public static void main(String args[]) {
+
+        outerloop:
+        for (int i = 1; i < 10; i++) {
+            System.out.print("\nOuter loop pass " + i + ", Inner loop: ");
+
+            for (int j = 1; j < 10; j++) {
+                if (j == 5) continue outerloop; // continue outerloop
+                System.out.print(j);
+            }
+        }
+    }
+
+}
