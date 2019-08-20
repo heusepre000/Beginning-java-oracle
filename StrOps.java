ac0d56b80bcb77f23e497aae5d9fa96fb12d7e2c
Preston  Heuser <37519044+heusepre000@users.noreply.github.com>
2019-08-20 12:31:25 -0700

    Add files via upload

---
+++ StrOps.java
@@ -0,0 +1,39 @@
+public class StrOps {
+    public static void main(String args[]) {
+        String str1 =
+                "When it comes to Web programming, Java is #1.";
+        String str2 = new String(str1);
+        String str3 = "Java strings are powerful.";
+        int result, idx;
+        char ch;
+
+        System.out.println("Length of str1: " + str1.length());
+
+        for (int i = 0; i < str1.length(); i++)
+            System.out.print(str1.charAt(i));
+        System.out.println();
+
+        if (str1.equals(str2))
+            System.out.println("str1 equals str2");
+        else
+            System.out.println("str1 does not equal str2");
+
+        if (str1.equals(str3))
+            System.out.println("str1 does not equal str3");
+
+        result = str1.compareTo(str3);
+        if (result < 0)
+            System.out.println("str1 and str3 are equal");
+        else if (result < 0)
+            System.out.println("str1 is less than str3");
+        else
+            System.out.println("str1 is greater than str3");
+
+        str2 = "One Two Three One";
+
+        idx = str2.indexOf("One");
+        System.out.println("Index of first ocurence of One: " + idx);
+        idx = str2.lastIndexOf("One");
+        System.out.println("Index of last occurnece of One: " + idx);
+    }
+}
